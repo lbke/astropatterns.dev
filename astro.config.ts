74fa9bd5922b32f94dfc7d035917c11a657b946a
eric-burel <eb@lbke.fr>
2024-10-15 20:21:33 +0200

    setup sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -3,11 +3,19 @@ import { defineConfig } from 'astro/config';
 
 import vercel from '@astrojs/vercel/serverless';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   devToolbar: {
     enabled: false,
   },
-  integrations: [tutorialkit()],
+  integrations: [
+    tutorialkit(),
+    // NOTE: sitemap-index.xml will be created as a static asset when building
+    // it might not be available during dev
+    // @see https://github.com/stackblitz/tutorialkit/pull/375
+    sitemap()
+  ],
   output: "hybrid",
   adapter: vercel({
     webAnalytics: { enabled: true }
